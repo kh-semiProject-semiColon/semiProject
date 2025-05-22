16114b64d985bbe55234c713a9088d1cb851c387
unknown <s413625@gmail.com>
2025-05-22 16:31:21 +0900

    스터디 생성기능 추가

--- SemiProject/src/main/java/kr/co/semi/main/controller/MainController.java
+++ SemiProject/src/main/java/kr/co/semi/main/controller/MainController.java
@@ -2,14 +2,17 @@ package kr.co.semi.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.co.semi.main.service.MainService;
 import kr.co.semi.studyboard.model.dto.Study;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class MainController {
 	
 	@Autowired
@@ -27,9 +30,15 @@ public class MainController {
 		return "study/study-creation";
 	}
 	
-	@RequestMapping("/study/creation")
+	/** 스터디 생성 by 김성원
+	 * @param study
+	 * @param ra
+	 * @return
+	 */
+	@PostMapping("/study/create")
 	@ResponseBody
 	public int stduyCreation(Study study, RedirectAttributes ra) {
+		log.info(study.toString());
 		int result = service.studyCreation(study);
 		return result;
 	}
