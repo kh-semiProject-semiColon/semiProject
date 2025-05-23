e68a7f88e62d17b39caafd32242a210c63be9524
DEEPBLUE104 <dlara7173@gmail.com>
2025-05-23 15:33:11 +0900

    자기소개 수정

--- SemiProject/src/main/java/kr/co/semi/myPage/model/service/MyPageServiceImpl.java
+++ SemiProject/src/main/java/kr/co/semi/myPage/model/service/MyPageServiceImpl.java
@@ -1,5 +1,43 @@
 package kr.co.semi.myPage.model.service;
 
-public class MyPageServiceImpl {
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.co.semi.member.model.dto.Member;
+import kr.co.semi.myPage.model.mapper.MyPageMapper;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+@PropertySource("classpath:/config.properties")
+public class MyPageServiceImpl implements MyPageService {
+
+	@Autowired
+	private MyPageMapper mapper;
+
+	@Override
+	public Member selectMember(int memberNo) {
+		return (Member) mapper.selectMember(memberNo);
+	}
+
+	@Override
+	public int updateInfo(Member inputMember) {
+
+		// 입력된 주소가 있을 경우
+		if (!inputMember.getMemberAddress().equals(",,")) {
+
+			String address = String.join("^^^", inputMember.getMemberAddress());
+			inputMember.setMemberAddress(address);
+
+		} else {
+			// 없을 경우
+			inputMember.setMemberAddress(null);
+		}
+
+		return mapper.updateInfo(inputMember);
+	}
 
 }
