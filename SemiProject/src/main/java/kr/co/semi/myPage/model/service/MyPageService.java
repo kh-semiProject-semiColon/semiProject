6001af9be353f657a2a177c21fcfd8274ad658af
DEEPBLUE104 <dlara7173@gmail.com>
2025-05-28 17:34:39 +0900

    수정

--- SemiProject/src/main/java/kr/co/semi/myPage/model/service/MyPageService.java
+++ SemiProject/src/main/java/kr/co/semi/myPage/model/service/MyPageService.java
@@ -3,6 +3,8 @@ package kr.co.semi.myPage.model.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import kr.co.semi.board.model.dto.Board;
 import kr.co.semi.board.model.dto.Pagination;
 import kr.co.semi.member.model.dto.Member;
@@ -14,7 +16,7 @@ public interface MyPageService {
 	Member selectMember(int memberNo);
 
 	// 회원정보 update문 메서드
-	int updateInfo(Member inputMember);
+	public int updateInfo(Member inputMember, MultipartFile profileImg);
 
 	// 비밀번호 조회하러 가는 길
 	boolean checkPassword(int memberNo, String inputPw);
